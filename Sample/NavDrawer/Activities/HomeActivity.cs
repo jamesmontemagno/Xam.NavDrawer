a01bf6570e40a3c3432c7e50a05a76345cd40e73
James Montemagno <james.montemagno@gmail.com>
2013-09-05 19:10:44 -0700

    some updates
    Signed-off-by: James Montemagno <james.montemagno@gmail.com>

--- Sample/NavDrawer/Activities/HomeActivity.cs
+++ Sample/NavDrawer/Activities/HomeActivity.cs
@@ -15,11 +15,13 @@ namespace NavDrawer.Activities
     [Activity(Label = "Home", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,Theme = "@style/MyTheme", Icon = "@drawable/ic_launcher")]
     public class HomeView : FragmentActivity
 	{
-        private DrawerLayout _drawer;
-        private MyActionBarDrawerToggle _drawerToggle;
-        private string _drawerTitle;
-        private string _title;
-        private ListView _drawerList;
+        
+        private MyActionBarDrawerToggle m_DrawerToggle;
+        private string m_DrawerTitle;
+        private string m_Title;
+
+        private DrawerLayout m_Drawer;
+        private ListView m_DrawerList;
         private static readonly string[] Sections = new[]
             {
                 "Browse", "Friends", "Profile"
@@ -28,51 +30,77 @@ namespace NavDrawer.Activities
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
             SetContentView(Resource.Layout.page_home_view);
 
-            this._title = this._drawerTitle = this.Title;
-            this._drawer = this.FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            this._drawerList = this.FindViewById<ListView>(Resource.Id.left_drawer);
+            this.m_Title = this.m_DrawerTitle = this.Title;
 
-            this._drawer.SetDrawerShadow(Resource.Drawable.drawer_shadow_dark, (int)GravityFlags.Start);
+            this.m_Drawer = this.FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
+            this.m_DrawerList = this.FindViewById<ListView>(Resource.Id.left_drawer);
 
-            this.ActionBar.SetDisplayHomeAsUpEnabled(true);
-            this.ActionBar.SetHomeButtonEnabled(true);
+            this.m_DrawerList.Adapter = new ArrayAdapter<string>(this, Resource.Layout.item_menu, Sections);
+            this.m_DrawerList.ItemClick += DrawerListOnItemClick;
 
-            //DrawerToggle is the animation that happens with the indicator next to the
-            //ActionBar icon. You can choose not to use this.
-            this._drawerToggle = new MyActionBarDrawerToggle(this, this._drawer,
+
+            this.m_Drawer.SetDrawerShadow(Resource.Drawable.drawer_shadow_dark, (int)GravityFlags.Start);
+
+
+
+            //DrawerToggle is the animation that happens with the indicator next to the actionbar
+            this.m_DrawerToggle = new MyActionBarDrawerToggle(this, this.m_Drawer,
                                                       Resource.Drawable.ic_drawer_light,
                                                       Resource.String.drawer_open,
                                                       Resource.String.drawer_close);
 
-            //You can alternatively use _drawer.DrawerClosed here
-            this._drawerToggle.DrawerClosed += delegate
+            //Display the current fragments title and update the options menu
+            this.m_DrawerToggle.DrawerClosed += delegate
             {
-                this.ActionBar.Title = this._title;
+                this.ActionBar.Title = this.m_Title;
                 this.InvalidateOptionsMenu();
             };
 
-
-            //You can alternatively use _drawer.DrawerOpened here
-            this._drawerToggle.DrawerOpened += delegate
+            //Display the drawer title and update the options menu
+            this.m_DrawerToggle.DrawerOpened += delegate
             {
-                this.ActionBar.Title = this._drawerTitle;
+                this.ActionBar.Title = this.m_DrawerTitle;
                 this.InvalidateOptionsMenu();
             };
 
-            _drawerList.Adapter = new ArrayAdapter<string>(this, Resource.Layout.item_menu, Sections);
+            //Set the drawer lister to be the toggle.
+            this.m_Drawer.SetDrawerListener(this.m_DrawerToggle);
 
-            this._drawer.SetDrawerListener(this._drawerToggle);
-
-            _drawerList.ItemClick += DrawerListOnItemClick;
+            
 
             //if first time you will want to go ahead and click first item.
             if (null == savedInstanceState)
             {
                 DrawerListOnItemClick(null, new AdapterView.ItemClickEventArgs(null, null, 0, 0));
             }
+
+
+            this.ActionBar.SetDisplayHomeAsUpEnabled(true);
+            this.ActionBar.SetHomeButtonEnabled(true);
+        }
+
+        protected override void OnPostCreate(Bundle savedInstanceState)
+        {
+            base.OnPostCreate(savedInstanceState);
+            this.m_DrawerToggle.SyncState();
+        }
+
+        public override void OnConfigurationChanged(Configuration newConfig)
+        {
+            base.OnConfigurationChanged(newConfig);
+            this.m_DrawerToggle.OnConfigurationChanged(newConfig);
+        }
+
+        // Pass the event to ActionBarDrawerToggle, if it returns
+        // true, then it has handled the app icon touch event
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            if (this.m_DrawerToggle.OnOptionsItemSelected(item))
+                return true;
+
+            return base.OnOptionsItemSelected(item);
         }
 
         private void DrawerListOnItemClick(object sender, AdapterView.ItemClickEventArgs itemClickEventArgs)
@@ -95,47 +123,23 @@ namespace NavDrawer.Activities
                 .Replace(Resource.Id.content_frame, fragment)
                 .Commit();
 
-            _drawerList.SetItemChecked(itemClickEventArgs.Position, true);
-            ActionBar.Title = _title = Sections[itemClickEventArgs.Position];
-            _drawer.CloseDrawer(_drawerList);
-        }
-
-        protected override void OnPostCreate(Bundle savedInstanceState)
-        {
-            base.OnPostCreate(savedInstanceState);
-            this._drawerToggle.SyncState();
+            this.m_DrawerList.SetItemChecked(itemClickEventArgs.Position, true);
+            ActionBar.Title = this.m_Title = Sections[itemClickEventArgs.Position];
+            this.m_Drawer.CloseDrawer(this.m_DrawerList);
         }
 
 
-        public override void OnConfigurationChanged(Configuration newConfig)
-        {
-            base.OnConfigurationChanged(newConfig);
-            this._drawerToggle.OnConfigurationChanged(newConfig);
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            //MenuInflater.Inflate(Resource.Menu.main, menu);
-            return base.OnCreateOptionsMenu(menu);
-        }
 
         public override bool OnPrepareOptionsMenu(IMenu menu)
         {
-            var drawerOpen = this._drawer.IsDrawerOpen(this._drawerList);
+
+            var drawerOpen = this.m_Drawer.IsDrawerOpen(this.m_DrawerList);
             //when open down't show anything
             for (int i = 0; i < menu.Size(); i++)
                 menu.GetItem(i).SetVisible(!drawerOpen);
 
 
             return base.OnPrepareOptionsMenu(menu);
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            if (this._drawerToggle.OnOptionsItemSelected(item))
-                return true;
-
-            return base.OnOptionsItemSelected(item);
         }
 
 	}
