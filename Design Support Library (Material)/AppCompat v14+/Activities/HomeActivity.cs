e08810e011aebac5dc76ff4c903b9f75ae0b3961
James Montemagno <james.montemagno@gmail.com>
2016-08-10 11:07:29 -0700

    Update so sub items are checked fixes #13

--- Design Support Library (Material)/AppCompat v14+/Activities/HomeActivity.cs
+++ Design Support Library (Material)/AppCompat v14+/Activities/HomeActivity.cs
@@ -10,88 +10,103 @@ using UniversalImageLoader.Core;
 
 namespace NavDrawer.Activities
 {
-	[Activity (Label = "Nav Drawer", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, Icon = "@drawable/ic_launcher")]
-	public class HomeView : BaseActivity
-	{
-		DrawerLayout drawerLayout;
-		NavigationView navigationView;
+    [Activity(Label = "Nav Drawer", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, Icon = "@drawable/ic_launcher")]
+    public class HomeView : BaseActivity
+    {
+        DrawerLayout drawerLayout;
+        NavigationView navigationView;
+        IMenuItem previousItem;
 
-		protected override int LayoutResource {
-			get {
-				return Resource.Layout.page_home_view;
-			}
-		}
+        protected override int LayoutResource
+        {
+            get
+            {
+                return Resource.Layout.page_home_view;
+            }
+        }
 
-		protected override void OnCreate (Bundle savedInstanceState)
-		{
-			base.OnCreate (savedInstanceState);
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
             var config = ImageLoaderConfiguration.CreateDefault(ApplicationContext);
             // Initialize ImageLoader with configuration.
             ImageLoader.Instance.Init(config);
 
-			drawerLayout = FindViewById<DrawerLayout> (Resource.Id.drawer_layout);
+            drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
 
-			SupportActionBar.SetHomeAsUpIndicator (Resource.Drawable.ic_menu);
-			navigationView = FindViewById<NavigationView> (Resource.Id.nav_view);
-			navigationView.NavigationItemSelected += (sender, e) => {
-				e.MenuItem.SetChecked (true);
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.ic_menu);
+            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
 
-				switch(e.MenuItem.ItemId)
-				{
-				case Resource.Id.nav_home:
-					ListItemClicked(0);
-					break;
-				case Resource.Id.nav_friends:
-					ListItemClicked(1);
-					break;
-				case Resource.Id.nav_profile:
-					ListItemClicked(2);
-					break;
-				}
+                if (previousItem != null)
+                    previousItem.SetChecked(false);
 
-				
+                navigationView.SetCheckedItem(e.MenuItem.ItemId);
 
-				drawerLayout.CloseDrawers ();
-			};
+                previousItem = e.MenuItem;
 
-			//if first time you will want to go ahead and click first item.
-			if (savedInstanceState == null) {
-				ListItemClicked (0);
-			}
-		}
+                switch (e.MenuItem.ItemId)
+                {
+                    case Resource.Id.nav_home:
+                        ListItemClicked(0);
+                        break;
+                    case Resource.Id.nav_friends:
+                        ListItemClicked(1);
+                        break;
+                    case Resource.Id.nav_profile:
+                        ListItemClicked(2);
+                        break;
+                }
 
-		private void ListItemClicked (int position)
-		{
-			Android.Support.V4.App.Fragment fragment = null;
-			switch (position) {
-			case 0:
-				fragment = new BrowseFragment ();
-				break;
-			case 1:
-				fragment = new FriendsFragment ();
-				break;
-			case 2:
-				fragment = new ProfileFragment ();
-				break;
-			}
 
-			SupportFragmentManager.BeginTransaction ()
-				.Replace (Resource.Id.content_frame, fragment)
-				.Commit ();
-		}
 
-	
+                drawerLayout.CloseDrawers();
+            };
 
-		public override bool OnOptionsItemSelected (IMenuItem item)
-		{
-			switch (item.ItemId)
-			{
-			case Android.Resource.Id.Home:
-				drawerLayout.OpenDrawer (Android.Support.V4.View.GravityCompat.Start);
-				return true;
-			}
-			return base.OnOptionsItemSelected (item);
-		}
-	}
+            //if first time you will want to go ahead and click first item.
+            if (savedInstanceState == null)
+            {
+                ListItemClicked(0);
+                navigationView.SetCheckedItem(Resource.Id.nav_home);
+            }
+        }
+
+       
+
+        private void ListItemClicked(int position)
+        {
+            Android.Support.V4.App.Fragment fragment = null;
+            switch (position)
+            {
+                case 0:
+                    fragment = new BrowseFragment();
+                    break;
+                case 1:
+                    fragment = new FriendsFragment();
+                    break;
+                case 2:
+                    fragment = new ProfileFragment();
+                    break;
+            }
+
+            SupportFragmentManager.BeginTransaction()
+                .Replace(Resource.Id.content_frame, fragment)
+                .Commit();
+        }
+
+
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            switch (item.ItemId)
+            {
+                case Android.Resource.Id.Home:
+                    drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
+                    return true;
+            }
+            return base.OnOptionsItemSelected(item);
+        }
+    }
 }
 
